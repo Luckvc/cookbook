00df970d1bdff9dcca5d39febd17e85247f1af16
Lucas Vasques <lucasvc1996@gmail.com>
2023-12-13 15:55:22 -0300

    final commit

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -4,6 +4,6 @@ class Api::V1::RecipesController < ActionController::API
     query = params["query"]
     result_set = Recipe.joins(:recipe_type, :user)
     result_set = result_set.where("title LIKE ? OR name LIKE ?", "%#{query}%", "%#{query}%")
-    render json: result_set.to_json(), status: 200
+    render json: result_set.to_json(only: [:title, :cook_time, "recipe_type.name", "user.email"]), status: 200
   end
 end
